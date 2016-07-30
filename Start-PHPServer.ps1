e1021db4c973376f011cf436c6cda5a50e94b469
DCAG <digiclick@hotmail.com>
2016-07-30 15:24:40 +0300

    connection to mongodb

---
+++ Start-PHPServer.ps1
@@ -0,0 +1,8 @@
+﻿Set-Location "$env:USERPROFILE\Documents\GitHub\RESTTutorial03\src"
+
+start-job {
+    Start-Sleep -Seconds 1
+    Start-Process 'http://127.0.0.1:8000/hello/world'
+    }
+    
+php.exe -S 127.0.0.1:8000 -t public public/index.php
